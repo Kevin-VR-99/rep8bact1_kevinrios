c92dfb61c39f5166db0f6f2ca9d9fec74bb6453b
Kevin-VR-99 <kevinvelazquezrios@gmail.com>
2026-02-26 10:46:15 -0600

    Creacion del index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" href="/REPO_AILTO/favicon.ico" type="image/x-icon">
+  <title>Kevin's page</title>
+</head>
+<body>
+  <h1>"Bienvenido mi pagina para praticar el uso de Git"</h1>
+  <br>
+  <br>
+  <a href="./acercade.php">Ir a Acerca de</a>
+</body>
+</html>
