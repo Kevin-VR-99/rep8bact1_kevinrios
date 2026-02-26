4511fbd52092df397ddc8de1227cd8f6a72ef781
Kevin-VR-99 <kevinvelazquezrios@gmail.com>
2026-02-26 11:08:29 -0600

    se creo el parte de acerca de

---
+++ acercade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/REP8BACT1_KEVINRIOS/favicon.ico" type="image/x-icon">
+    <title>Ailton's page</title>
+</head>
+<body>
+
+    <h1>"Bienvenido a mi pagina acerda de"</h1>
+    <br>
+
+    <h2>"Esta es otra parte de la pagina"</h2>
+    <br>
+
+    <img src="images/fotoa.jpg" alt="Mi imagen" width="300">
+    <br><br>
+
+    <a href="./index.php">Ir a la pagina principal</a>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="icon" href="/REPO_AILTO/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="/REP8BACT1_KEVINRIOS/favicon.ico" type="image/x-icon">
   <title>Kevin's page</title>
 </head>
 <body>
